30fa04609c49d2a5562fbb72cc5534e5d527b10f
ELindquist <npgolfleague@gmail.com>
2018-05-26 21:44:58 -0400

    Added home and help pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  get  'static_pages/home'
+  get  'static_pages/help'
   resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'static_pages#home'
   match '/:id' => 'pages#show', :via => [:get], :as => 'page_show'
   
 end
